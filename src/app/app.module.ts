73528f82f5f2557f2405f1e36def3827a1ce0a10
Andrea <andreaelbittarl@gmail.com>
2022-12-14 16:40:44 -0500

    act-component-about-us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ElectronicComponent } from './electronic/electronic.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ElectronicComponent],
+  declarations: [AppComponent, HeaderComponent, ElectronicComponent, AboutUsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
